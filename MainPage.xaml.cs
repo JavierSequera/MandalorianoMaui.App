663e8448a090c26852603fd97c432fee4dea0d76
Javier Sequera <javisequera19@gmail.com>
2022-11-09 14:42:31 +0100

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MandalorianoMaui;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+}
+
---
+++ ViewModels/MandalorianoVM.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandalorianoMaui.ViewModels
+{
+    public class MandalorianoVM
+    {
+        public ObservableCollection<clsMision> ListaMisiones { get;}
+
+        public MandalorianoVM(ObservableCollection<clsMision> ListaMisiones)
+        {
+            this.ListaMisiones = ListaMisiones; 
+        }
+
+        public MandalorianoVM()
+        {
+            ListaMisiones = clsListaMisionesBL.obtenerListaMisiones();
+        }
+
+    }
+}
