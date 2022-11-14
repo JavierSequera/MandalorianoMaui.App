80b64dac0720f38a6cbbbe4ca46f08d248b6fdbf
Javier Sequera <javisequera19@gmail.com>
2022-11-14 13:31:05 +0100

    SDDAS

--- ViewModels/MandalorianoVM.cs
+++ ViewModels/MandalorianoVM.cs
@@ -1,5 +1,6 @@
 ﻿using DAL;
 using Entidades;
+using MandalorianoMaui.ViewModels.Utilidades;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,19 +10,93 @@ using System.Threading.Tasks;
 
 namespace MandalorianoMaui.ViewModels
 {
-    public class MandalorianoVM
+    public class MandalorianoVM:clsVMBase
     {
-        public ObservableCollection<clsMision> ListaMisiones { get;}
+        #region Atributos
+        private ObservableCollection<clsMision> listaMisiones; 
 
-        public MandalorianoVM(ObservableCollection<clsMision> ListaMisiones)
-        {
-            this.ListaMisiones = ListaMisiones; 
+        private clsMision misionSeleccionada;
+
+        private DelegateCommand mostrarDetallesCommand;
+
+        private Boolean datosMisionEsVisible;
+        #endregion
+
+        #region propiedades
+
+        public ObservableCollection<clsMision> ListaMisiones { 
+
+            get { return listaMisiones; } 
+            set { listaMisiones = value; }}
+
+        public clsMision MisionSeleccionada { 
+
+            get { return misionSeleccionada; } 
+            set {
+                if (misionSeleccionada != value)
+                {
+                    misionSeleccionada = value;
+                    mostrarDetallesCommand.RaiseCanExecuteChanged();
+                    DatosMisionEsVisible = false;
+                    NotifyPropertyChanged(nameof(datosMisionEsVisible));
+                }
+            }     
         }
+
+        public DelegateCommand MostrarDetallesCommand {
+
+            get {
+                mostrarDetallesCommand = new DelegateCommand(mostrarContenidoMision_execute, mostrarContenidoMision_canExecute);
+                return mostrarDetallesCommand;
+            } }
+
+        public Boolean DatosMisionEsVisible { 
+
+            get { return datosMisionEsVisible; } 
+            set { datosMisionEsVisible = value; NotifyPropertyChanged(); } }
+
+       
+
+
+        #endregion
+
+        #region Constructores
 
         public MandalorianoVM()
         {
             ListaMisiones = clsListaMisionesBL.obtenerListaMisiones();
         }
 
+        #endregion
+
+        #region Comandos
+        /// <summary>
+        /// Método que muestra la información de una misión seleccionada haciendo visible el texto mediante booleano.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void mostrarContenidoMision_execute()
+        {
+            DatosMisionEsVisible = true;
+            NotifyPropertyChanged("datosMisionEsVisible");
+            NotifyPropertyChanged("misionSeleccionada");
+
+        }
+        /// <summary>
+        /// Método que cambia el estado del botón.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private bool mostrarContenidoMision_canExecute()
+        {
+            bool mostrar = false;
+            if (MisionSeleccionada != null)
+            {
+                mostrar = true;
+            }
+            return mostrar;
+        }
+        #endregion
     }
+
 }
+
